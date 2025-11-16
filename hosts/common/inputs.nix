fb0301e5ad216ab15b5bb01d9d4f8214aad26a53
Hayley Sparham <5777950+PartlyAwesome@users.noreply.github.com>
2025-11-16 18:24:26 +0000

    make inputs.nix even better through the use of nix. add x280 keyboard to the config

--- hosts/common/inputs.nix
+++ hosts/common/inputs.nix
@@ -17,26 +17,27 @@
         (keyMappings // {HOLD_START = "BEFORE_CONSUME";})
       ];
     });
+    keyboards = [
+      "AT Translated Set 2 keyboard"
+      "Keychron K8 Keychron K8"
+    ];
+    job = "${pkgs.interception-tools}/bin/intercept -g $DEVNODE | ${pkgs.interception-tools-plugins.dual-function-keys}/bin/dual-function-keys -c ${dfkConfig} | ${pkgs.interception-tools}/bin/uinput -d $DEVNODE";
+    createConfigs = map (name: {
+      JOB = job;
+      DEVICE = {
+        NAME = name;
+        EVENTS = {
+          EV_KEY = [
+            (builtins.attrValues keyMappings)
+          ];
+        };
+      };
+    });
   in {
     enable = true;
     plugins = with pkgs.interception-tools-plugins; [
       dual-function-keys
     ];
-    udevmonConfig = builtins.toJSON [
-      {
-        JOB = "${pkgs.interception-tools}/bin/intercept -g $DEVNODE | ${pkgs.interception-tools-plugins.dual-function-keys}/bin/dual-function-keys -c ${dfkConfig} | ${pkgs.interception-tools}/bin/uinput -d $DEVNODE";
-        # device names can be found by doing:
-        # enter interception-tools shell: nix-shell -p interception-tools
-        # run: sudo uinput -p -d /dev/input/by-id/[TAB]
-        DEVICE = {
-          NAME = "Keychron K8 Keychron K8";
-          EVENTS = {
-            EV_KEY = [
-              (builtins.attrValues keyMappings)
-            ];
-          };
-        };
-      }
-    ];
+    udevmonConfig = builtins.toJSON (createConfigs keyboards);
   };
 }
