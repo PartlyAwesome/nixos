510a0e2d1d27b3435a65edef197b3823ccefe3a6
Hayley Sparham <5777950+PartlyAwesome@users.noreply.github.com>
2025-11-17 20:27:46 +0000

    added some nix options

--- hosts/common/lix.nix
+++ hosts/common/lix.nix
@@ -1,19 +1,29 @@
-{ config, pkgs, ... }:
-
 {
-  nixpkgs.overlays = [ (final: prev: {
-    inherit (prev.lixPackageSets.stable)
-      nixpkgs-review
-      nix-eval-jobs
-      nix-fast-build
-      colmena;
-  }) ];
+  config,
+  pkgs,
+  ...
+}: {
+  nixpkgs.overlays = [
+    (final: prev: {
+      inherit
+        (prev.lixPackageSets.stable)
+        nixpkgs-review
+        nix-eval-jobs
+        nix-fast-build
+        colmena
+        ;
+    })
+  ];
 
   nix.package = pkgs.lixPackageSets.stable.lix;
 
   nix.settings = {
-    experimental-features = [ "nix-command" "flakes" ];
-    #download-buffer-size = 524288000; not on lix
+    experimental-features = ["nix-command" "flakes"];
+    connect-timeout = 30;
+    min-free = 128000000;
+    max-free = 1000000000;
+    auto-optimise-store = true;
+    narinfo-cache-negative-ttl = 0;
   };
 
   nixpkgs.config.allowUnfree = true;
