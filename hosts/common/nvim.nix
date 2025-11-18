42fdb6bbb7fbd0a3779406aaa98f8535f622ce4d
Hayley Sparham <5777950+PartlyAwesome@users.noreply.github.com>
2025-11-18 00:54:57 +0000

    add guess-indent plugin to nvim

--- hosts/common/nvim.nix
+++ hosts/common/nvim.nix
@@ -1,6 +1,7 @@
 {
   config,
   inputs,
+  pkgs,
   ...
 }:
 
@@ -9,37 +10,45 @@
     nvf.nixosModules.default
   ];
 
-  programs.nvf = {
-    enable = true;
+  programs.nvf.enable = true;
+  programs.nvf.settings.vim = {
+    viAlias = true;
+    vimAlias = true;
 
-    settings.vim = {
-      viAlias = true;
-      vimAlias = true;
+    theme = {
+      enable = true;
+      name = "catppuccin";
+      style = "mocha";
+    };
 
-      theme = {
-        enable = true;
-        name = "catppuccin";
-        style = "mocha";
+    statusline.lualine.enable = true;
+    telescope.enable = true;
+    autocomplete.nvim-cmp.enable = true;
+
+    lazy.plugins = {
+      "guess-indent.nvim" = {
+        package = pkgs.vimPlugins.guess-indent-nvim;
+        setupModule = "guess-indent";
+        setupOpts = {
+          auto_cmd = true;
+          override_editorconfig = true;
+        };
       };
+    };
 
-      statusline.lualine.enable = true;
-      telescope.enable = true;
-      autocomplete.nvim-cmp.enable = true;
+    lsp = {
+      enable = true;
+      formatOnSave = true;
+    };
 
-      lsp = {
-        enable = true;
-        formatOnSave = true;
-      };
+    languages = {
+      enableDAP = true;
+      enableTreesitter = true;
+      enableFormat = true;
 
-      languages = {
-        enableDAP = true;
-        enableTreesitter = true;
-        enableFormat = true;
-
-        nix.enable = true;
-        ts.enable = true;
-        rust.enable = true;
-      };
+      nix.enable = true;
+      ts.enable = true;
+      rust.enable = true;
     };
   };
 }
