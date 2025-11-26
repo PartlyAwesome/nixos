a7143e6018ac936e2aca4a59f5ce20c9a9637858
Hayley Sparham <5777950+PartlyAwesome@users.noreply.github.com>
2025-11-26 10:47:32 +0000

    add HD6XX EQ pipewire module

---
+++ hosts/common/audio/hd6xx.eq
@@ -0,0 +1,6 @@
+Preamp: -10.06 dB
+Filter 1: ON LSC Fc 105.0 Hz Gain 14.1 dB Q 0.70
+Filter 2: ON PK Fc 58.4 Hz Gain -8.7 dB Q 0.42
+Filter 3: ON PK Fc 3167.4 Hz Gain -1.5 dB Q 3.71
+Filter 4: ON PK Fc 8799.6 Hz Gain 5.2 dB Q 1.50
+Filter 5: ON HSC Fc 10000.0 Hz Gain -0.3 dB Q 0.70
--- hosts/common/audio/pipewire-extra.nix
+++ hosts/common/audio/pipewire-extra.nix
@@ -1,27 +1,57 @@
-{
-  config,
-  ...
-}:
-
-{
-  services.pipewire.extraConfig.pipewire."10-null-sink" = {
-    "context.objects" = [
-      {
-        factory = "adapter";
-        args = {
-          "factory.name" = "support.null-audio-sink";
-          "node.name" = "my-sink";
-          "media.class" = "Audio/Sink";
-          "audio.position" = "FL,FR";
-          "monitor.channel-values" = "true";
-          "monitor.passthrough" = "true";
-          "adapter.auto-port-config" = {
-            "mode" = "dsp";
-            "monitor" = "true";
-            "position" = "preserve";
+{...}: {
+  services.pipewire.extraConfig.pipewire = {
+    "10-null-sink" = {
+      "context.objects" = [
+        {
+          factory = "adapter";
+          args = {
+            "factory.name" = "support.null-audio-sink";
+            "node.name" = "my-sink";
+            "media.class" = "Audio/Sink";
+            "audio.position" = "FL,FR";
+            "monitor.channel-values" = "true";
+            "monitor.passthrough" = "true";
+            "adapter.auto-port-config" = {
+              "mode" = "dsp";
+              "monitor" = "true";
+              "position" = "preserve";
+            };
           };
-        };
-      }
-    ];
+        }
+      ];
+    };
+    "15-para-eq" = {
+      "context.modules" = [
+        {
+          name = "libpipewire-module-parametric-equalizer";
+          args = {
+            "equalizer.filepath" = ./hd6xx.eq;
+            "equalizer.description" = "HD6XX EQ";
+            "audio.channels" = "2";
+            "audio.position" = "FL,FR";
+            "capture.props" = {
+              "node.name" = "HD6XX EQ Input";
+            };
+            "playback.props" = {
+              "node.name" = "HD6XX EQ Output";
+            };
+          };
+        }
+      ];
+    };
+    #"20-link-null-sink" = {
+    #  "context.objects" = [
+    #    {
+    #      factory = "link-factory";
+    #      arg = {
+    #        "link.output.node" = "my-sink";
+    #        "link.output.port" = "output_FL";
+    #        "link.input.node" = "system";
+    #        "link.input.port" = "playback_1";
+    #        "link.passive" = "true";
+    #      };
+    #    }
+    #  ];
+    #};
   };
 }
