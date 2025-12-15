90b329949c2ac1a174fa11dd768154ca2cf70569
Hayley Sparham <5777950+PartlyAwesome@users.noreply.github.com>
2025-12-15 09:27:50 +0000

    update flakes. add binary cache. still doesn't build

--- flake.nix
+++ flake.nix
@@ -6,11 +6,13 @@
       "https://nix-community.cachix.org"
       "https://colmena.cachix.org"
       "https://cache.garnix.io"
+      "https://attic.xuyh0120.win/lantian"
     ];
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "colmena.cachix.org-1:7BzpDnjjH8ki2CT3f6GdOk7QAzPOl+1t3LvTLXqYcSg="
       "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+      "https://github.com/xddxdd/nix-cachyos-kernel"
     ];
   };
 
