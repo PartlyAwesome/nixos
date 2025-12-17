39dbd60014ccf0c17a663445c36f266483c2371a
Hayley Sparham <5777950+PartlyAwesome@users.noreply.github.com>
2025-12-17 15:02:08 +0000

    allow remote ssh access!

--- hosts/common/ssh.nix
+++ hosts/common/ssh.nix
@@ -1,8 +1,15 @@
 {
+  lib,
+  hostsPath,
+  pkgs,
   config,
   user,
   ...
-}: {
+}: let
+  port = lib.toIntBase10 (builtins.readFile (hostsPath "common/secrets/openssh-port.secret"));
+  x280-key-file = pkgs.writeText "x280.pub" (builtins.readFile (hostsPath "keys/x280.pub"));
+  main-key-file = pkgs.writeText "main.pub" (builtins.readFile (hostsPath "keys/main.pub"));
+in {
   # sops.secrets = {
   #   "openssh-port" = {
   #     owner = config.users.users.${user}.name;
@@ -10,17 +17,25 @@
   #   };
   # };
   programs.ssh.startAgent = true;
-  # services.openssh = {
-  #   enable = true;
-  #   ports = [
-  #     (builtins.readFile config.sops.secrets."openssh-port".path)
-  #   ];
-  #   settings = {
-  #     PasswordAuthentication = false;
-  #     KbdInteractiveAuthentication = false;
-  #     PermitRootLogin = "no";
-  #     AllowUsers = [user];
-  #   };
-  # };
-  # services.fail2ban.enable = true;
+  services.openssh = {
+    enable = true;
+    ports = [
+      port
+    ];
+    settings = {
+      PasswordAuthentication = false;
+      KbdInteractiveAuthentication = false;
+      PermitRootLogin = "no";
+      AllowUsers = [user];
+    };
+    authorizedKeysFiles = [
+      "${x280-key-file}"
+      "${main-key-file}"
+    ];
+  };
+  users.users.${user}.openssh.authorizedKeys.keyFiles = [
+    x280-key-file
+    main-key-file
+  ];
+  services.fail2ban.enable = true;
 }
