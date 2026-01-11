214b67fb3284d06b487da4e6adaad38fcf2c6cbd
Hayley Sparham <5777950+PartlyAwesome@users.noreply.github.com>
2026-01-11 19:55:55 +0000

    set serif font to roboto serif

--- hosts/common/fonts.nix
+++ hosts/common/fonts.nix
@@ -4,12 +4,13 @@
     packages = with pkgs; [
       nerd-fonts.roboto-mono
       roboto-flex
+      roboto-serif
     ];
 
     fontDir.enable = true;
 
     fontconfig.defaultFonts = {
-      serif = ["Roboto Flex"];
+      serif = ["Roboto Serif"];
       sansSerif = ["Roboto Flex"];
       monospace = ["RobotoMono Nerd Font"];
     };
