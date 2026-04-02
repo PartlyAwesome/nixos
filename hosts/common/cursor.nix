652beb7f60c782ac6d621a252fef139b4d2a028e
Hayley Sparham <5777950+PartlyAwesome@users.noreply.github.com>
2026-04-02 21:03:01 +0100

    fix(cursor): double down and set xdg.icons true and add posy as the fallback cursor theme

--- hosts/common/cursor.nix
+++ hosts/common/cursor.nix
@@ -6,13 +6,18 @@
   stdenvNoCC = pkgs.stdenvNoCC;
   fetchFromGitHub = pkgs.fetchFromGitHub;
   posy-scalable = pkgs.callPackage ./posy-scalable.nix {inherit lib stdenvNoCC fetchFromGitHub;};
+  theme-name = "posys_cursor_scalable_mono_black";
 in {
   environment.systemPackages = with pkgs; [xsettingsd xrdb posy-scalable];
   home-pkgs = [posy-scalable];
+  xdg.icons = {
+    enable = true; # it is enabled by default, but there is never any harm in doubling down
+    fallbackCursorThemes = [theme-name];
+  };
   hm.services.xsettingsd.enable = true;
   hm.home.pointerCursor = {
     enable = true;
-    name = "posys_cursor_scalable_mono_black";
+    name = theme-name;
     package = posy-scalable;
 
     dotIcons.enable = true;
