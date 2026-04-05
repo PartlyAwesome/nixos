8fac867f7d2fae567c3564bc6eca0c84cabe1327
Hayley Sparham <5777950+PartlyAwesome@users.noreply.github.com>
2026-04-06 00:07:45 +0100

    feat(cursor): use posy-cursor overlay

--- hosts/common/cursor.nix
+++ hosts/common/cursor.nix
@@ -3,11 +3,11 @@
   pkgs,
   ...
 }: let
-  posy-scalable = inputs.posy-cursor.packages.${pkgs.stdenv.hostPlatform.system}.default;
   theme-name = "posys_cursor_scalable_mono_black";
 in {
+  nixpkgs.overlays = [inputs.posy-cursor.overlays.default];
   environment.systemPackages = with pkgs; [xsettingsd xrdb posy-scalable];
-  home-pkgs = [posy-scalable];
+  home-pkgs = with pkgs; [posy-scalable];
   xdg.icons = {
     enable = true; # it is enabled by default, but there is never any harm in doubling down
     fallbackCursorThemes = [theme-name];
@@ -16,7 +16,7 @@ in {
   hm.home.pointerCursor = {
     enable = true;
     name = theme-name;
-    package = posy-scalable;
+    package = pkgs.posy-scalable;
 
     dotIcons.enable = true;
     gtk.enable = true;
