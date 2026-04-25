0ec81a7562354ad3fb87edd6666e3bac4f811001
Hayley Sparham <5777950+PartlyAwesome@users.noreply.github.com>
2026-04-25 14:58:47 +0100

    fix(git): add default branch name

--- hosts/common/git.nix
+++ hosts/common/git.nix
@@ -4,6 +4,7 @@
     settings = {
       user.name = "Hayley Sparham";
       user.email = "hayley@partlyaweso.me";
+      init.defaultBranch = "master";
       push.autoSetupRemote = true;
     };
 
