050b94425cbb718fbca64b9c4adb3e265e1cc24e
Hayley Sparham <5777950+PartlyAwesome@users.noreply.github.com>
2026-05-03 05:22:21 +0100

    feat(lix): import lix-module not as a flake because I couldn't figure out how you're meant to change the lix source using the module. move lix related stuff to lix subdirectory

--- flake.nix
+++ flake.nix
@@ -34,11 +34,8 @@
     nixos-core.inputs.nixpkgs.follows = "nixpkgs";
     lix.url = "https://git.lix.systems/lix-project/lix/archive/main.tar.gz";
     lix.flake = false;
-    lix-module = {
-      url = "https://git.lix.systems/lix-project/nixos-module/archive/main.tar.gz";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.lix.follows = "lix";
-    };
+    lix-module.url = "https://git.lix.systems/lix-project/nixos-module/archive/main.tar.gz";
+    lix-module.flake = false;
   };
 
   outputs = inputs: {
---
+++ hosts/common/lix/default.nix
@@ -0,0 +1,45 @@
+{
+  inputs,
+  lib,
+  ...
+}: let
+  lixVersionJson = builtins.fromJSON (builtins.readFile (inputs.lix + "/version.json"));
+  versionSuffix = with inputs;
+    lib.optionalString (!lixVersionJson.official_release)
+    "-pre${builtins.substring 0 8 lix.lastModifiedDate or lix.lastModified or "19700101"}-dev_${lix.shortRev or lix.dirtyShortRev or "dirty"}-raf-patched";
+in {
+  # nixpkgs.overlays = [
+  #   (final: prev: {
+  #     inherit
+  #       (prev.lixPackageSets.latest)
+  #       nixpkgs-review
+  #       nix-eval-jobs
+  #       nix-fast-build
+  #       colmena
+  #       ;
+  #   })
+  # ];
+  # imports = [inputs.lix-module.nixosModules.default];
+  imports = [
+    (lib.modules.importApply "${inputs.lix-module}/nixos-module.nix" {
+      inherit versionSuffix;
+      lix = inputs.lix;
+    })
+  ];
+
+  # nix.package = pkgs.lixPackageSets.latest.lix;
+
+  nix.settings = {
+    experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
+    connect-timeout = 30;
+    min-free = 128000000;
+    max-free = 1000000000;
+    auto-optimise-store = true;
+    narinfo-cache-negative-ttl = 0;
+  };
+
+  nixpkgs.config.allowUnfree = true;
+}
