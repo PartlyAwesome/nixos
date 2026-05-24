29d358585d3b52b84043de38b7ae3278433647e8
Hayley Sparham <5777950+PartlyAwesome@users.noreply.github.com>
2026-05-24 12:15:29 +0100

    fix(tailscale): manually set tailscale dns entry, fixes issue where accepting tailscale dns causes SSL issues

--- hosts/common/tailscale.nix
+++ hosts/common/tailscale.nix
@@ -2,6 +2,14 @@
   services.tailscale = {
     enable = true;
     extraUpFlags = ["--login-server=https://head.to.partlyaweso.me"];
+    extraSetFlags = ["--accept-dns=false"];
     useRoutingFeatures = "client";
+  };
+  services.resolved.dnsDelegates.tailscale.Delegate = {
+    DNS = [
+      "100.100.100.100"
+      "fd7a:115c:a1e0::53"
+    ];
+    Domains = ["on.partlyaweso.me"];
   };
 }
