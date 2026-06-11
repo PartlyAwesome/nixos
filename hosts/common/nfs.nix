3483012d4dbe4db67824369b3ed62773613ed5c9
Hayley Sparham <5777950+PartlyAwesome@users.noreply.github.com>
2026-06-11 01:38:41 +0100

    fix(nfs): hopefully make NFS more reliable

--- hosts/common/nfs.nix
+++ hosts/common/nfs.nix
@@ -3,11 +3,15 @@
     device = "where-shit-goes:/volume1";
     fsType = "nfs";
     options = [
-      "nfsvers=4.1"
+      "nfsvers=4.2"
       "x-systemd.automount"
       "noauto"
-      "x-systemd.idle-timeout=3600"
+      "x-systemd.idle-timeout=900"
+      "forcedirectio"
+      "largefiles"
+      "tcp"
     ];
   };
   boot.supportedFilesystems = ["nfs"];
+  services.rpcbind.enable = true;
 }
