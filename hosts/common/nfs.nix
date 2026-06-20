4f5131027d084ba4085e2d1c50269439c7a47d9b
Hayley Sparham <5777950+PartlyAwesome@users.noreply.github.com>
2026-06-20 01:03:29 +0100

    fix(nfs): probably the best it's going to get

--- hosts/common/nfs.nix
+++ hosts/common/nfs.nix
@@ -3,12 +3,15 @@
     device = "where-shit-goes:/volume1";
     fsType = "nfs";
     options = [
-      "nfsvers=4.2"
+      "nfsvers=4.1"
       "x-systemd.automount"
       "noauto"
       "x-systemd.idle-timeout=900"
-      "forcedirectio"
-      "largefiles"
+      "x-systemd.device-timeout=5s"
+      "x-systemd.mount-timeout=5s"
+      "_netdev"
+      # "forcedirectio"
+      # "largefiles"
       "tcp"
     ];
   };
