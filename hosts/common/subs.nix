c54995f2c4b6523988c3b4de7a54eb4fb6799c7c
Hayley Sparham <5777950+PartlyAwesome@users.noreply.github.com>
2026-08-01 11:05:46 +0100

    fix(subs): remove garnix as it is dead

--- hosts/common/subs.nix
+++ hosts/common/subs.nix
@@ -2,13 +2,11 @@
   defaultSubstituters = [
     "https://nix-community.cachix.org"
     "https://colmena.cachix.org"
-    "https://cache.garnix.io"
     "https://attic.xuyh0120.win/lantian"
   ];
   defaultKeys = [
     "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     "colmena.cachix.org-1:7BzpDnjjH8ki2CT3f6GdOk7QAzPOl+1t3LvTLXqYcSg="
-    "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
     "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
   ];
 in {
