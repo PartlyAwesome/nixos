ae103161771ed13744921c06b090acccceaaad3e
Hayley Sparham <5777950+PartlyAwesome@users.noreply.github.com>
2026-09-03 17:24:05 +0100

    feat(steam): add protontricks, extest (wayland steam-input), local game transfers, remoteplay and dedicated server enable/openFirewall options

--- hosts/common/gaming/steam.nix
+++ hosts/common/gaming/steam.nix
@@ -1,6 +1,11 @@
 {pkgs, ...}: {
   programs.steam = {
     enable = true;
+    protontricks.enable = true;
     extraCompatPackages = [pkgs.proton-ge-bin];
+    localNetworkGameTransfers.openFirewall = true;
+    remotePlay.openFirewall = true;
+    extest.enable = true;
+    dedicatedServer.openFirewall = true;
   };
 }
