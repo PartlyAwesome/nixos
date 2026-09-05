cc056d95a1414923093d0ee9f5f870de31b77f1f
Hayley Sparham <5777950+PartlyAwesome@users.noreply.github.com>
2026-09-05 22:07:40 +0100

    refactor(catppuccin): merge catppuccin nixos/home configurations

--- hosts/common/catppuccin.nix
+++ hosts/common/catppuccin.nix
@@ -1,11 +1,19 @@
-{inputs, ...}: {
-  imports = [
-    inputs.catppuccin.nixosModules.default
-  ];
-  catppuccin = {
+{inputs, ...}: let
+  defaults = {
     enable = true;
+    autoEnable = true;
     cache.enable = true;
     flavor = "mocha";
     accent = "pink";
   };
+in {
+  imports = [inputs.catppuccin.nixosModules.default];
+  hm.imports = [inputs.catppuccin.homeModules.catppuccin];
+  catppuccin = defaults;
+  hm.catppuccin =
+    defaults
+    // {
+      vesktop.enable = false;
+      cursors.enable = false;
+    };
 }
